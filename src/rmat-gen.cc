8109887e8e7ec6eb0b237a064868deb4d94d7133
Yuhang Zhou <zyhvg77@gmail.com>
2023-09-20 12:28:43 +0000

    init

---
+++ src/rmat-gen.cc
@@ -0,0 +1,27 @@
+// Copyright (c) 2015, The Regents of the University of California (Regents)
+// See LICENSE.txt for license details
+
+#include <iostream>
+#include <vector>
+
+#include "benchmark.h"
+#include "bitmap.h"
+#include "builder.h"
+#include "command_line.h"
+#include "graph.h"
+#include "platform_atomics.h"
+#include "pvector.h"
+#include "sliding_queue.h"
+#include "timer.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  CLApp cli(argc, argv, "breadth-first search");
+  if (!cli.ParseArgs())
+    return -1;
+  Builder b(cli);
+  Graph g = b.MakeGraph();
+  g.PrintStats();
+  return 0;
+}
